1584c4e98acd33a300285bfecd5fe274cc8ef984
hrusikesh8280 <hrusikeshviroot@gmail.com>
2023-10-26 18:01:07 +0530

    web 3.0 icp token

--- src/token_backend/main.mo
+++ src/token_backend/main.mo
@@ -13,6 +13,8 @@ actor Token {
   var balances = HashMap.HashMap<Principal,Nat>(1,Principal.equal,Principal.hash);
   balances.put(owner,totalSupply);
 
+
+
   public query func balanceOf(who:Principal):async Nat{
 
     let balance :Nat = switch(balances.get(who)){
@@ -27,11 +29,11 @@ actor Token {
 
   public shared(msg) func payOut(): async Text{
 
-     Debug.print(debug_show(msg.caller));
+    Debug.print(debug_show(msg.caller));
     if(balances.get(msg.caller)==null) {
     let amount = 10000;
-    balances.put(msg.caller,amount);
-    return "Success";
+    let result = await transfer(msg.caller,amount);
+    return result;
     }else{
       return "Alredy Claimed"
     };
@@ -39,4 +41,19 @@ actor Token {
    
     // return "Success";
   };
+  public shared(msg) func transfer(to:Principal ,amount:Nat): async Text{
+    let fromBalance = await balanceOf(msg.caller);
+    if(fromBalance > amount){
+      let newFromBalance :Nat = fromBalance - amount;
+      balances.put(msg.caller,newFromBalance);
+
+      let toBalance = await balanceOf(to);
+      let newTobalance = toBalance + amount ;
+      balances.put(to,newTobalance);
+      return "Success" ;
+    }else{
+      return "Insufficient Funds"
+    };
+
+  }
 };
--- src/token_frontend/src/components/Transfer.jsx
+++ src/token_frontend/src/components/Transfer.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React,{useState} from "react";
+import {token_backend} from "../../../declarations/token_backend"
+import {Principal} from "@dfinity/principal"
+
 
 function Transfer() {
+
+  const [recipentId,setId]=useState("")
+  const [amount,setAmount]=useState("")
+  const [isDisabled,setDisabled]=useState(false)
+  const [feedback,setFeedback]=useState("")
+  const [isHidden,setHidden]=useState(true)
+
   
   async function handleClick() {
-    
+    setHidden(true)
+    setDisabled(true)
+    // console.log(recipentId);
+    const recipient =  Principal.fromText(recipentId)
+    const amountToTransfer = Number(amount)
+    const result = await token_backend.transfer(recipient,amountToTransfer)
+    setFeedback(result)
+    setHidden(false)
+    setDisabled(false)
+    // console.log("tranfer button clicked",recipient);
   }
 
   return (
@@ -16,6 +35,8 @@ function Transfer() {
               <input
                 type="text"
                 id="transfer-to-id"
+                value={recipentId}
+                onChange={(e)=>setId(e.target.value)}
               />
             </li>
           </ul>
@@ -27,15 +48,18 @@ function Transfer() {
               <input
                 type="number"
                 id="amount"
+                value={amount}
+                onChange={(e)=>setAmount(e.target.value)}
               />
             </li>
           </ul>
         </fieldset>
         <p className="trade-buttons">
-          <button id="btn-transfer" onClick={handleClick} >
+          <button id="btn-transfer" onClick={handleClick} disabled={isDisabled} >
             Transfer
           </button>
         </p>
+      <p hidden={isHidden}>{feedback}</p>
       </div>
     </div>
   );
